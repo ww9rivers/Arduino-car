2879304d49b105b6aeb481712c1d1345fd07b8e0
Wei Wang <ww@9rivers.com>
2019-04-14 12:13:02 -0400

    Renamed Combo/utils.h -> Combo/Combo.h

--- Combo/Combo.h
+++ Combo/Combo.h
@@ -3,6 +3,19 @@
  *  that are implemented in the main module, Combo.ino, but are used in the
  *  avoidance.cpp submodule.
  */
+#ifndef _COMBO_H_
+#define _COMBO_H_ 1
+
+//  Operation modes:
+typedef enum {
+  STOP_MODE,
+  AUTO_MODE,
+  IR_MODE,
+  AVOIDANCE_MODE,
+  TRACKING_MODE,
+  TESTING_MODE
+} Op_Mode;
+extern Op_Mode op_mode;
 
 /**
  *  Timer -- Macros for timer operation
@@ -10,8 +23,12 @@
 #define timer_init(x)       { x = millis(); }
 #define timer_exceeds(x, y) (millis()-x > y)
 
+void stateChange(void);
+
 void stop_car (void);
 void go_reverse(void);
 void go_forward(void);
 void turn_left(void);
 void turn_right(void);
+
+#endif
